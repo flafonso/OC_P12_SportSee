4cbb18a1fa2b9b3a060dbf930a6fb11d85f0fe88
Flavien Afonso <af.flavien@gmail.com>
2024-05-06 14:30:35 +0200

    Add a customizable tooltip, without typescript errors, for the daily activity graph

--- src/assets/CheeseburgerIcon.tsx
+++ src/assets/CheeseburgerIcon.tsx
@@ -16,8 +16,8 @@ function CheeseburgerIcon() {
         fill="#FD5181"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M 11.3 0 H 8.8 C 4.6 0 1.3 3.4 1.3 7.5 H 18.8 C 18.8 3.4 15.4 0 11.3 0 Z M 7.5 5 C 6.8 5 6.3 4.5 6.3 3.8 C 6.3 3 6.8 2.5 7.5 2.5 C 8.3 2.5 8.8 3 8.8 3.8 C 8.8 4.5 8.3 5 7.5 5 Z M 12.5 5 C 12.5 5.8 13 6.3 13.8 6.3 C 14.5 6.3 15 5.8 15 5 C 15 4.3 14.5 3.8 13.8 3.8 C 13 3.8 12.5 4.3 12.5 5 Z"
         fill="#FD5181"
       />
--- src/components/graphs/DailyActivityChart.tsx
+++ src/components/graphs/DailyActivityChart.tsx
@@ -5,38 +5,79 @@ import {
   Tooltip,
   Legend,
   Bar,
-  CartesianGrid
+  CartesianGrid,
+  TooltipProps,
 } from "recharts";
-import { Payload } from "recharts/types/component/DefaultLegendContent";
+// import { ValueType, NameType } from "recharts/types/component/DefaultTooltipContent";
 
 function DailyActivityChart({ data }: { data: any }) {
   console.log(data);
 
-  const renderTooltip = (custom: number) => {
-    // const { payload } = props;
-  
+  const renderTooltip = ({ active, payload }: TooltipProps<number, string>) => {
+    if (!(active && payload && payload.length === 2)) {
+      return null;
+    }
     return (
-      <ul>
-        {custom}
-      </ul>
+      <div className="custom-tooltip">
+        <p>{`${payload[0].value}kg`}</p>
+        <p>{`${payload[1].value}Kcal`}</p>
+      </div>
     );
-  }
+  };
 
   return (
     <div className="daily-activity-chart">
       <h3>Activité quotidienne</h3>
       {/* <ResponsiveContainer width="100%" height="100%" className="daily-activity-chart"> */}
-        <BarChart width={835} height={320} data={data} barSize={7} barGap={8} barCategoryGap={40} margin={{top: 23, right: 0, left: 23, bottom: 23}}>
-          <CartesianGrid strokeDasharray="3 3" vertical={false} />
-          <XAxis dataKey="day" />
-          <YAxis yAxisId="left" orientation="left" dataKey="calories" hide={true} />
-          <YAxis yAxisId="right" orientation="right" dataKey="kilogram" axisLine={false} domain={[75, 82]} />
-          <Tooltip viewBox={{x: 0, y: 0, width: 40, height: 60}} labelStyle={{ backgroundColor: 'red' }} content={renderTooltip(12)}/>
-          <Legend verticalAlign="top" align="right" iconType="circle" iconSize={8} height={70} wrapperStyle={{ right: 20 }}/>
-          <Bar name="Poids (kg)" yAxisId="right" dataKey="kilogram" fill="#282D30" radius={[3, 3, 0, 0]}/>
-          <Bar name="Calories brûlées (kCal)" yAxisId="left"dataKey="calories" fill="#E60000" radius={[3, 3, 0, 0]}/>
-        </BarChart>
-     {/* </ResponsiveContainer> */}
+      <BarChart
+        width={835}
+        height={320}
+        data={data}
+        barSize={7}
+        barGap={8}
+        barCategoryGap={40}
+        margin={{ top: 23, right: 0, left: 23, bottom: 23 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" vertical={false} />
+        <XAxis dataKey="day" />
+        <YAxis
+          yAxisId="left"
+          orientation="left"
+          dataKey="calories"
+          hide={true}
+        />
+        <YAxis
+          yAxisId="right"
+          orientation="right"
+          dataKey="kilogram"
+          axisLine={false}
+          domain={[75, 82]}
+        />
+        <Tooltip content={renderTooltip} />
+        <Legend
+          verticalAlign="top"
+          align="right"
+          iconType="circle"
+          iconSize={8}
+          height={70}
+          wrapperStyle={{ right: 20 }}
+        />
+        <Bar
+          name="Poids (kg)"
+          yAxisId="right"
+          dataKey="kilogram"
+          fill="#282D30"
+          radius={[3, 3, 0, 0]}
+        />
+        <Bar
+          name="Calories brûlées (kCal)"
+          yAxisId="left"
+          dataKey="calories"
+          fill="#E60000"
+          radius={[3, 3, 0, 0]}
+        />
+      </BarChart>
+      {/* </ResponsiveContainer> */}
     </div>
   );
 }

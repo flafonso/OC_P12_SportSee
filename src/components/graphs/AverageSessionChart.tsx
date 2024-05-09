c7ca8fc85d8f346eb8113394dfb0f198116e2edb
Flavien Afonso <af.flavien@gmail.com>
2024-05-09 14:46:02 +0200

    Adding the AverageSessionChart base with data and a bit of css

--- src/components/graphs/AverageSessionChart.tsx
+++ src/components/graphs/AverageSessionChart.tsx
@@ -1,6 +1,23 @@
-function AverageSessionChart({ data }: {data: any}) {
+import {
+  LineChart,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Line,
+  // TooltipProps,
+} from "recharts";
+
+function AverageSessionChart({ data }: { data: any }) {
+  console.log(data);
   return (
-    <div className="average-session-chart"></div>
+    <div className="average-session-chart">
+      <LineChart width={258} height={263} data={data}>
+        <XAxis dataKey="day" />
+        <YAxis dataKey="sessionLength" hide={false} domain={[-4, "dataMax"]} />
+        <Line type="monotone" dataKey="sessionLength" dot={false} />
+        <Tooltip />
+      </LineChart>
+    </div>
   );
 }
 

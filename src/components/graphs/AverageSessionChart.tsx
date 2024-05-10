e3229f4292bbd83df7d76de862ac6cae83b9b544
Flavien Afonso <af.flavien@gmail.com>
2024-05-10 03:37:21 +0200

    Progress on AverageSessionChart with the addition of the correct colors, dimensions and title

--- src/components/graphs/AverageSessionChart.tsx
+++ src/components/graphs/AverageSessionChart.tsx
@@ -4,18 +4,44 @@ import {
   YAxis,
   Tooltip,
   Line,
-  // TooltipProps,
+  TooltipProps,
 } from "recharts";
+import { ValueType, NameType } from "recharts/types/component/DefaultTooltipContent";
 
 function AverageSessionChart({ data }: { data: any }) {
   console.log(data);
+  const renderTooltip = ({ active, payload }: TooltipProps<ValueType, NameType>) => {
+    console.log(payload);
+    if (!(active && payload && payload.length)) {
+      return null;
+    }
+    return (
+      <div className="custom-tooltip">
+        <p>{`${payload[0].value} min`}</p>
+      </div>
+    );
+  };
+
   return (
     <div className="average-session-chart">
-      <LineChart width={258} height={263} data={data}>
-        <XAxis dataKey="day" />
-        <YAxis dataKey="sessionLength" hide={false} domain={[-4, "dataMax"]} />
-        <Line type="monotone" dataKey="sessionLength" dot={false} />
-        <Tooltip />
+      <h3>Durée moyenne des sessions</h3>
+      <LineChart width={258} height={263} data={data} margin={{ top: 77}}>
+        <XAxis dataKey="day" tickLine={false} axisLine={false} padding={{ left: 12, right: 12 }} />
+        <YAxis dataKey="sessionLength" hide={true} domain={[-4, "dataMax"]} />
+        <defs>
+          <linearGradient id="gradientLine">
+            <stop offset="0%" stopColor="#FFFFFF" stopOpacity={0.4} />
+            <stop offset="81%" stopColor="#FFFFFF" />
+          </linearGradient>
+        </defs>
+        <Line
+          type="monotone"
+          dataKey="sessionLength"
+          dot={false}
+          stroke="url(#gradientLine)"
+          strokeWidth={2}
+        />
+        <Tooltip content={renderTooltip} />
       </LineChart>
     </div>
   );

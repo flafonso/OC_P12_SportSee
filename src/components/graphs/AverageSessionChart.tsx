b25ef0f3967e2728c0f2b65eb5fa075e1bffd79b
Flavien Afonso <af.flavien@gmail.com>
2024-05-10 04:09:12 +0200

    Add a tickFormatter to AverageSessionChart to display day letters on the x-axis

--- src/components/graphs/AverageSessionChart.tsx
+++ src/components/graphs/AverageSessionChart.tsx
@@ -1,17 +1,15 @@
+import { LineChart, XAxis, YAxis, Tooltip, Line, TooltipProps } from "recharts";
 import {
-  LineChart,
-  XAxis,
-  YAxis,
-  Tooltip,
-  Line,
-  TooltipProps,
-} from "recharts";
-import { ValueType, NameType } from "recharts/types/component/DefaultTooltipContent";
+  ValueType,
+  NameType,
+} from "recharts/types/component/DefaultTooltipContent";
 
 function AverageSessionChart({ data }: { data: any }) {
   console.log(data);
-  const renderTooltip = ({ active, payload }: TooltipProps<ValueType, NameType>) => {
-    console.log(payload);
+  const renderTooltip = ({
+    active,
+    payload,
+  }: TooltipProps<ValueType, NameType>) => {
     if (!(active && payload && payload.length)) {
       return null;
     }
@@ -22,16 +20,41 @@ function AverageSessionChart({ data }: { data: any }) {
     );
   };
 
+  const convertNbrToDay: Record<number, string> = {
+    1: "L",
+    2: "M",
+    3: "M",
+    4: "J",
+    5: "V",
+    6: "S",
+    7: "D",
+  };
+
+  const foramtXAxis = (tick: number) => {
+    return tick >= 1 && tick <= 7 ? convertNbrToDay[tick] : String(tick);
+  };
+
   return (
     <div className="average-session-chart">
       <h3>Durée moyenne des sessions</h3>
-      <LineChart width={258} height={263} data={data} margin={{ top: 77}}>
-        <XAxis dataKey="day" tickLine={false} axisLine={false} padding={{ left: 12, right: 12 }} />
+      <LineChart
+        width={258}
+        height={263}
+        data={data}
+        margin={{ top: 77, bottom: 10 }}
+      >
+        <XAxis
+          dataKey="day"
+          tickLine={false}
+          tickFormatter={foramtXAxis}
+          axisLine={false}
+          padding={{ left: 14, right: 14 }}
+        />
         <YAxis dataKey="sessionLength" hide={true} domain={[-4, "dataMax"]} />
         <defs>
           <linearGradient id="gradientLine">
             <stop offset="0%" stopColor="#FFFFFF" stopOpacity={0.4} />
-            <stop offset="81%" stopColor="#FFFFFF" />
+            <stop offset="100%" stopColor="#FFFFFF" stopOpacity={0.88} />
           </linearGradient>
         </defs>
         <Line

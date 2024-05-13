2ed5ef53424d04e179849302e4771e350d53eef0
Flavien Afonso <af.flavien@gmail.com>
2024-05-13 17:35:04 +0200

    Creation of the ActivityTypeChart component that receives the correct data, and creation of its scss file

--- src/components/graphs/ActivityTypeChart.tsx
+++ src/components/graphs/ActivityTypeChart.tsx
@@ -1,1 +1,29 @@
 //comment
+import {
+  RadarChart,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Line,
+  TooltipProps,
+  DotProps,
+} from "recharts";
+// import {
+//   ValueType,
+//   NameType,
+// } from "recharts/types/component/DefaultTooltipContent";
+
+function ActivityTypeChart({ performanceData }: { performanceData: any }) {
+  // console.log(performanceData);
+  const { kind, data } = performanceData;
+  console.log(kind);
+  console.log(data);
+
+  return (
+    <div className="activity-type-chart">
+      {/* <RadarChart width={258} height={263} data={data}></RadarChart> */}
+    </div>
+  );
+}
+
+export default ActivityTypeChart;

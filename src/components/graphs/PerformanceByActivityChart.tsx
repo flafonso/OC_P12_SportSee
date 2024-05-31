c212736668fe574c0b27f8983db9dd6479257de3
Flavien Afonso <af.flavien@gmail.com>
2024-05-31 08:08:22 +0200

    Add latest changes to PerformanceByActivityChart

--- src/components/graphs/PerformanceByActivityChart.tsx
+++ src/components/graphs/PerformanceByActivityChart.tsx
@@ -4,29 +4,60 @@ import {
   PolarGrid,
   PolarAngleAxis,
   // PolarRadiusAxis,
-  Radar
+  Radar,
 } from "recharts";
 // import {
 //   ValueType,
 //   NameType,
 // } from "recharts/types/component/DefaultTooltipContent";
 
-function PerformanceByActivityChart({ performanceData }: { performanceData: any }) {
+function PerformanceByActivityChart({
+  performanceData,
+}: {
+  performanceData: any;
+}) {
   // console.log(performanceData);
   const { kind, data } = performanceData;
   // console.log(kind);
   // console.log(data);
-  const kindNbrToStr = (props: {value: number, kind: number}) => {
-    console.log(props);
-    return kind[props.kind];
+  let reversedData = [];
+  for(let i = data.length - 1; i >= 0; i--) {
+    reversedData.push(data[i]);
   }
+  console.log(reversedData);
+
+  const kindTranslated: {[key: string]: string} = {
+    "cardio": "Cardio",
+    "energy": "Energie",
+    "endurance": "Endurance",
+    "strength": "Force",
+    "speed": "Vitesse",
+    "intensity": "Intensité",
+  };
+
+  const kindNbrToStr = (props: { value: number; kind: number }) => {
+    // console.log(props);
+    const kindStr: string = kind[props.kind] || "";
+    if (kindTranslated[kindStr]) {
+      return kindTranslated[kindStr];
+    }
+    return props.kind;
+  };
 
   return (
     <div className="performance-by-activity">
-      <RadarChart width={258} height={263} data={data}>
-        <PolarGrid polarRadius={[12.2, 25, 50, 75, 100]} radialLines={false} stroke="#FFFFFF"/>
-        <PolarAngleAxis dataKey={kindNbrToStr} />
-        <Radar dataKey="value" fill="#FF0101" fillOpacity={0.7}/>
+      <RadarChart outerRadius={90} width={258} height={263} data={reversedData}>
+        <PolarGrid
+          polarRadius={[11.25, 22.5, 45, 67.5, 90]}
+          radialLines={false}
+          stroke="#FFFFFF"
+        />
+        <PolarAngleAxis
+          dataKey={kindNbrToStr}
+          // dataKey="kind"
+          tick={{ fontSize: 12, fontWeight: 500, fill: "#FFFFFF" }}
+        />
+        <Radar dataKey="value" fill="#FF0101" fillOpacity={0.7} />
       </RadarChart>
     </div>
   );

70dc99cc833e9fb6714a9d02598122c6eedebf68
Flavien Afonso <af.flavien@gmail.com>
2024-06-14 04:11:48 +0200

    Dashboard component modified to use api service for data

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,38 +7,77 @@ import Sidebar from "../components/Sidebar";
 import DailyActivityChart from "../components/graphs/DailyActivityChart.js";
 import PerformanceByActivityChart from "../components/graphs/PerformanceByActivityChart.tsx";
 import AverageSessionChart from "../components/graphs/AverageSessionChart.js";
-
-import {
-  USER_MAIN_DATA,
-  USER_ACTIVITY,
-  USER_AVERAGE_SESSIONS,
-  USER_PERFORMANCE
-} from "../mockedData.js";
 import ScoreChart from "../components/graphs/ScoreChart.tsx";
+import {
+  UserData,
+  UserActivity,
+  UserAverageSessions,
+  UserPerformance,
+} from "../dataNormalizer.ts";
 
-//comment test
+import { useEffect, useState } from "react";
+import {
+  fetchUserData,
+  fetchUserActivity,
+  fetchUserAverageSessions,
+  fetchUserPerformance,
+} from "../services/apiService.ts";
 
 function Dashboard() {
-  console.log(USER_ACTIVITY[0].sessions);
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [userActivity, setUserActivity] = useState<UserActivity | null>(null);
+  const [userAverageSessions, setUserAverageSessions] =
+    useState<UserAverageSessions | null>(null);
+  const [userPerformance, setUserPerformance] =
+    useState<UserPerformance | null>(null);
+  const userId = 12;
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const [mainData, activity, averageSessions, performance] =
+          await Promise.all([
+            fetchUserData(userId),
+            fetchUserActivity(userId),
+            fetchUserAverageSessions(userId),
+            fetchUserPerformance(userId),
+          ]);
+        setUserData(mainData);
+        setUserActivity(activity);
+        setUserAverageSessions(averageSessions);
+        setUserPerformance(performance);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+      }
+    }
+    fetchData();
+  }, [userId]);
+
+  if (!userData || !userActivity || !userAverageSessions || !userPerformance) {
+    return <div>Loading...</div>;
+  }
+  // console.log(userData);
+  // console.log(userActivity);
+  // console.log(userAverageSessions);
+  // console.log(userPerformance);
+
   return (
     <div className="dashboard">
       <Sidebar />
       <div className="dashboard__content">
         <h1>
           Bonjour{" "}
-          <span className="user-name">
-            {USER_MAIN_DATA[0].userInfos.firstName}
-          </span>
+          <span className="user-name">{userData.userInfos.firstName}</span>
         </h1>
         <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
 
         <div className="user-infos">
           <div className="user-infos__graphs">
-            <DailyActivityChart data={USER_ACTIVITY[0].sessions} />
+            <DailyActivityChart data={userActivity.sessions} />
             <div className="activity-details">
-              <AverageSessionChart data={USER_AVERAGE_SESSIONS[0].sessions} />
-              <PerformanceByActivityChart performanceData={USER_PERFORMANCE[0]} />
-              <ScoreChart data={USER_MAIN_DATA[0]} />
+              <AverageSessionChart data={userAverageSessions.sessions} />
+              <PerformanceByActivityChart performanceData={userPerformance} />
+              <ScoreChart data={userData} />
             </div>
           </div>
 
@@ -46,7 +85,7 @@ function Dashboard() {
             <NutrimentInfo
               typeName="Calories"
               unit="kCal"
-              value={USER_MAIN_DATA[0].keyData.calorieCount}
+              value={userData.keyData.calorieCount}
               backgroundColor="#FF000010"
             >
               <EnergyIcon />
@@ -54,7 +93,7 @@ function Dashboard() {
             <NutrimentInfo
               typeName="Proteines"
               unit="g"
-              value={USER_MAIN_DATA[0].keyData.proteinCount}
+              value={userData.keyData.proteinCount}
               backgroundColor="#4AB8FF10"
             >
               <ChickenIcon />
@@ -62,7 +101,7 @@ function Dashboard() {
             <NutrimentInfo
               typeName="Glucides"
               unit="g"
-              value={USER_MAIN_DATA[0].keyData.carbohydrateCount}
+              value={userData.keyData.carbohydrateCount}
               backgroundColor="#F9CE2310"
             >
               <AppleIcon />
@@ -70,7 +109,7 @@ function Dashboard() {
             <NutrimentInfo
               typeName="Lipides"
               unit="g"
-              value={USER_MAIN_DATA[0].keyData.lipidCount}
+              value={userData.keyData.lipidCount}
               backgroundColor="#FD518110"
             >
               <CheeseburgerIcon />
